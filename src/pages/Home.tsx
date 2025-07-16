ae213a45dcd5e997120fbd762edca9334935c879
yeoeunyoon <yoonyeoeun040706@gmail.com>
2025-07-16 17:29:35 +0900

    add text on home:

---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="home-page">
+      {/* Hey, I'm Jasmine! */}
+      <h1 className="home-title">
+        Hey, I'm Jasmine!
+      </h1>
+      
+      {/* 3D Avatar Image */}
+      <div className="avatar-container">
+        <img 
+          src="/src/images/avatar1.png" 
+          alt="Jasmine's 3D Avatar" 
+          className="avatar-image"
+        />
+      </div>
+      
+      {/* Wanna know more about me? */}
+      <h2 className="home-subtitle">
+        Wanna know more about me?
+      </h2>
+      
+      {/* Click under the bar or tap right */}
+      <div className="instruction-text">
+        Click under the bar or tap right
+      </div>
+    </div>
+  );
+};
+
+export default Home;
